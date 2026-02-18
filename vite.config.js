3197950f9c9c2c3361eaa9c3502bd958b372d3f8
Adrianhf17 <adrianhuablocho@gmail.com>
2026-02-18 15:18:34 -0500

    Prueba_nube

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: ""
+  base: "/2026-0.LoginApp-master/"
 })
